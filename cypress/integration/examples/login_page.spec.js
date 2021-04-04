0baed618f4437e0379c8907af574ce30a74b1f69
Kailash Pathak <kailash.pathak@3pillarglobal.com>
2021-04-04 10:14:37 +0530

    test case description change

--- cypress/integration/examples/login_page.spec.js
+++ cypress/integration/examples/login_page.spec.js
@@ -1,18 +1,15 @@
 /// <reference types="cypress" />
 
-describe("Test login funcationlity ", () => {
-    it("Open URL", () => {
-      cy.visit("http://www.testyou.in/Login.aspx");
-    });
-    it("Login Into Application", () => {
-        cy.get('#ctl00_CPHContainer_txtUserLogin').type('kailash1980');
-        cy.get('#ctl00_CPHContainer_txtPassword').type('Sonie8088');
-        cy.get('#ctl00_CPHContainer_btnLoginn').click();
-
-      });
-      it("Logout From Application", () => {
-        cy.get('#ctl00_headerTopStudent_lnkbtnSignout').click();
-      });
+describe("Test login funcationlity : login into  the application and logout ", () => {
+  it("Open URL", () => {
+    cy.visit("http://www.testyou.in/Login.aspx");
   });
-
-  
+  it("Login Into Application", () => {
+    cy.get("#ctl00_CPHContainer_txtUserLogin").type("kailash1980");
+    cy.get("#ctl00_CPHContainer_txtPassword").type("Sonie8088");
+    cy.get("#ctl00_CPHContainer_btnLoginn").click();
+  });
+  it("Logout From Application", () => {
+    cy.get("#ctl00_headerTopStudent_lnkbtnSignout").click();
+  });
+});
